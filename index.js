037e63f403eccbdc21242897b0252f34b37d9187
username_anjastabuni <reblytabuni2004@gmail.com>
2025-01-29 17:54:52 +0700

    passing data ke template ejs

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/t/:tag", (req, res) => {
+  const { tag } = req.params;
+  res.render("tag", { tag });
+});
+
+app.get("/rand", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { random: num });
 });
 
 app.listen(8080, () => {
